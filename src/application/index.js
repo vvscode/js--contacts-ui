b31b3fad74efaeebcb1c50e469b496af20c0405c
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-02-16 18:49:38 +0400

    Setup Pages

--- src/application/index.js
+++ src/application/index.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
-import RegistrationForm from '../components/RegistrationForm';
+import RegisterPage from '../pages/register/index';
+import ContactsPage from '../pages/contacts/index';
+import LoginPage from '../pages/login/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <RegistrationForm />
+      <Router>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/contacts" component={ContactsPage} />
+        <Route component={() => <Redirect to="/login" />} />
+        </Switch>
+      </Router>
       </div>
     );
   }
---
+++ src/pages/contacts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ContactsPage extends React.Component {
+  render() {
+    return <h1>Contacts</h1>
+  }
+}
+
+export default ContactsPage;
